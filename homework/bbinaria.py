3c4669fed6a706d7f28638cb1f251a04b857d3ea
HectorMtz22 <hec.gre81@gmail.com>
2021-10-28 10:35:42 -0500

    My numbers

--- homework/bbinaria.py
+++ homework/bbinaria.py
@@ -1,5 +1,6 @@
 def bbinaria(ordenados, buscado):
     n = len(ordenados)
+    print("Tengo", n, ordenados, "y busco por", buscado)
     if n == 0: # no hay nada
         return False
     pos = n // 2 # div. entera
@@ -11,4 +12,12 @@ def bbinaria(ordenados, buscado):
     else: # pivote < buscado # viene desp. del pivote
         return bbinaria(ordenados[pos + 1 :], buscado)
 
-print(bbinaria([x for x in range(4, 30, 5)], 15))
+# print(bbinaria([x for x in range(4, 30, 5)], 15))
+
+desde = 4
+hasta = 63
+paso = 3
+ubicar = 12
+ordenados = [97, 99, 10, 44, 15, 80, 56, 91, 60, 9]
+
+print(bbinaria([x for x in range(desde, hasta + 1, paso)], ubicar))
